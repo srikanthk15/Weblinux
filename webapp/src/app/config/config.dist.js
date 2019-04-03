24398370b65681a59d99dd9352a8b4a737db313a
Srikanth Koyili <srikanth.koyili@gmail.com>
2019-04-03 13:18:10 -0600

    Update config files to load from local respository.

--- webapp/src/app/config/config.dist.js
+++ webapp/src/app/config/config.dist.js
@@ -1,3 +1,2 @@
-//export const jor1kBaseFsUrl = 'https://cs-education.github.io/sysassets/jor1kfs/sysroot/or1k/';
-export const jor1kBaseFsUrl = 'jor1k/Openrisc-sys/';
+export const jor1kBaseFsUrl = '../bower_modules/jor1k/Openrisc-sys/';
 export const jor1kWorkerUrl = 'jor1k-worker.js';
